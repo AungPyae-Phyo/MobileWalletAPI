9bc3675b367a852ba282bce5192e4ae91a1f1755
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-28 22:29:13 +0630

    Application Setup

---
+++ Application/DTOs/UserDTO/UserRegistrationDto.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Domain.Contracts;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.UserDTO;
+
+public class UserRegistrationDto
+{
+    public string Name { get; set; }
+
+    public string? PhoneNumber { get; set; } = default!;
+
+    public string? Email { get; set; } = default!;
+
+    public string? Role { get; set; } = Enum.GetName(typeof(Role), Domain.Enums.Role.None);
+
+    public string? HashPassword { get; set; } = default!;
+}
+public class UserDtoProfile : Profile
+{
+    public UserDtoProfile()
+    {
+        CreateMap<UserRegistrationDto, User>()
+           // .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+           .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+           .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+           .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+           .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
+           .ForMember(dest => dest.HashPassword, opt => opt.MapFrom(src => src.HashPassword));
+    }
+}
---
+++ Application/Services/UserService.cs
@@ -0,0 +1,75 @@
+﻿using Application.DTOs.UserDTO;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Contracts;
+using Infrastructure.GenericRepo;
+using Infrastructure.IRepository;
+using Infrastructure.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services;
+public class UserService: IUserService
+{
+    private readonly IUnit _unit;
+    private readonly IGenericRepository<User, string> _genericRepository;
+    private readonly IUserRepo _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserService(IUnit unit, IUserRepo userRepository, IMapper mapper)
+    {
+        _unit = unit;
+        _genericRepository = _unit.GetRepository<User, string>();
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<int> RegisterUserAsync(UserRegistrationDto userRegistrationDto)
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(userRegistrationDto.Email))
+            {
+                throw new ArgumentException("Email cannot be empty.", nameof(userRegistrationDto.Email));
+            }
+
+            var existingUser = await _userRepository.FindByEmailAsync(userRegistrationDto.Email);
+
+            if (existingUser != null)
+            {
+                Console.WriteLine("Email is already in use.");
+                return -1;
+            }
+
+            var user = _mapper.Map<User>(userRegistrationDto);
+            user.Id = Guid.NewGuid().ToString().ToUpper();
+
+            user.HashPassword = BCrypt.Net.BCrypt.HashPassword(userRegistrationDto.HashPassword);
+
+            int result = await _genericRepository.Add(user);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error registering user: {ex.Message}");
+            return -1;
+        }
+    }
+
+
+
+
+    //public async Task<User> GetById(string id)
+    //{
+    //    var user = await _genericRepository.Get(id);
+    //    if (user == null)
+    //        throw new Exception("Member record does not exist.");
+    //    return user;
+    //}
+
+
+}
