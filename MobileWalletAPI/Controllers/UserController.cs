b176123ba67c936c95e6faac70f23a7f565b6a78
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-28 22:30:09 +0630

    testing: Register API

---
+++ MobileWalletAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTOs.UserDTO;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MobileWalletAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+      
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterCustomer([FromBody] UserRegistrationDto userRegistrationDto)
+        {
+            int response = await _service.RegisterUserAsync(userRegistrationDto);
+            return Created("", new { result = (response > 0) });
+
+        }
+
+
+    }
+}
+
