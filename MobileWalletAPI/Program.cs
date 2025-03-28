e0648d377397e4df97b795bc688e46a92a5f28c3
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-28 22:30:13 +0630

    Create Program.cs

---
+++ MobileWalletAPI/Program.cs
@@ -0,0 +1,39 @@
+using Application.Interfaces;
+using Application.Services;
+using Domain.Database;
+using Infrastructure.IRepository;
+using Infrastructure.Repository;
+using Infrastructure.UnitOfWork;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddScoped<ApplicationDbContext>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddSwaggerGen(); // OpenAPI is already configured with SwaggerGen
+
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUnit, Unit>();
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); // Ensure authentication middleware is added if authentication is used
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
