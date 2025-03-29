3e4fa01d3b876b1c972b1c025759bffe1f2ea4f7
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-29 21:18:06 +0630

    create: Bankstatuses Enum

---
+++ Domain/Enums/Status.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum BankStatus
+    {
+        //Basic statuses
+        NEW_USER,
+        CLOSED,
+
+        //Verification statuses
+        ACTIVE,
+        PENDING,
+        INACTIVE,   
+        ON_HOLD,
+        SUSPENDED,
+        APPROVE,
+        REJECT,
+        BLOCK,
+
+        // Technical statuses
+        UNDER_MAINTENANCE,
+
+        // Security-related statuses
+        LOCKED,
+        FROZEN,
+    }
+}
