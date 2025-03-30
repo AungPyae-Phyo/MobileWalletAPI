564b451a76711f5d19293afb9556017e9864bda9
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-30 23:07:12 +0630

    Create IAuthService.cs

---
+++ Application/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.AuthResponse;
+using Application.DTOs.UserDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> LoginUserAsync(UserLoginDto userDto);
+
+        Task<int> RefreshToken(AuthResponse tokenRequest);
+    }
+}
