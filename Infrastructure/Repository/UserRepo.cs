f3dbc2c8910689e74b9b3f5ecb4c42995df93e65
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-30 23:08:43 +0630

    Update UserRepo.cs

--- Infrastructure/Repository/UserRepo.cs
+++ Infrastructure/Repository/UserRepo.cs
@@ -3,20 +3,18 @@ using Domain.Contracts;
 using Domain.Database;
 using Infrastructure.GenericRepository;
 using Infrastructure.IRepository;
-
-
 namespace Infrastructure.Repository;
 
-    public class UserRepo:GenericRepository<User, string>, IUserRepo
+public class UserRepo : GenericRepository<User, string>, IUserRepo
+{
+    public UserRepo(ApplicationDbContext context) : base(context)
     {
-        public UserRepo (ApplicationDbContext context) :base (context)
-        {
 
-        }
+    }
     public async Task<User> GetUserByRefreshToken(string refreshToken)
     {
-        string query = "SELECT * FROM Users WHERE refresh_token = @RefreshToken";
-        return await _connection.QueryFirstOrDefaultAsync<User>(query, new { RefreshToken = refreshToken});
+        string query = "SELECT * FROM Users WHERE RefreshToken = @RefreshToken";
+        return await _connection.QueryFirstOrDefaultAsync<User>(query, new { RefreshToken = refreshToken });
     }
 
     public async Task<User> FindByEmailAsync(string email)
@@ -27,7 +25,14 @@ namespace Infrastructure.Repository;
         return await _connection.QueryFirstOrDefaultAsync<User>(query, new { Email = email.ToLower() });
     }
 
+    public async Task<bool> UpdateRefreshToken(string userId, string refreshToken, DateTime expiryDate)
+    {
+        string query = $"UPDATE {TABLE_NAME} SET RefreshToken = @RefreshToken, RefreshTokenExpiry = @ExpiryDate WHERE id = @UserId";
 
+        int rowAffected = await _connection.ExecuteAsync(query, new { RefreshToken = refreshToken, ExpiryDate = expiryDate, UserId = userId });
+
+        return rowAffected > 0;
+    }
 
 }
 
