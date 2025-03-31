5fc9e66a82f14b641a9c86451880f02c7377c03f
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-31 23:43:40 +0630

    Create WalletController.cs

---
+++ MobileWalletAPI/Controllers/WalletController.cs
@@ -0,0 +1,39 @@
+﻿using Application.DTOs.WalletDTO;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace MobileWalletAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WalletController : ControllerBase
+    {
+        private readonly IWalletService _walletService;
+
+        public WalletController(IWalletService walletService)
+        {
+            _walletService = walletService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateWallet([FromBody] WalletDTO walletDto)
+        {
+            try
+            {
+                var result = await _walletService.Create(walletDto);
+                if (result > 0)
+                {
+                    return Ok(new { message = "Wallet created successfully" });
+                }
+                return BadRequest(new { message = "Failed to create wallet" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+    }
+}
