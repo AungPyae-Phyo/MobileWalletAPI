a9fb5bf7fbc0403eefb579484098e59ddb5bddff
AungPyae-Phyo <pyaea978@gmail.com>
2025-03-31 23:43:59 +0630

    Create IWalletService.cs

---
+++ Application/Interfaces/IWalletService.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs.WalletDTO;
+using Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IWalletService
+    {
+        Task<int> Create(WalletDTO walletDto);
+        Task<int> UpdateBalance(string userId, decimal amount);
+        Task<WalletDTO> Get(string userId);
+        Task<IEnumerable<Wallet>> GetAll();
+        Task<int> Update(WalletDTO walletDto);
+        Task<bool> SoftDelete(string userId);
+        Task<int> CountAll();
+
+
+    }
+}
