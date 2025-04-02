4e8960c0db4a48a0e37e381ec4cb2d9332381ae1
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-03 02:43:14 +0630

    Update IUnit.cs

--- Infrastructure/UnitOfWork/IUnit.cs
+++ Infrastructure/UnitOfWork/IUnit.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities;
 using Infrastructure.GenericRepository;
+using Infrastructure.IRepository;
 
 
 namespace Infrastructure.UnitOfWork
@@ -7,6 +8,6 @@ namespace Infrastructure.UnitOfWork
     public interface IUnit
     {
         GenericRepository<T, TId> GetRepository<T, TId>() where T : class, IEntity<TId>;
-
+        IKYCRepo KYCRepo { get; }
     }
 }
