93c36cd799cefd0e6f8bf2a99e5bf7aac1b552df
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-04 02:31:14 +0630

    create: helper method for GenerateWalletAcount

---
+++ Application/Helper/GenerateWalletAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Application.Helper
+{
+    public static class GenerateWalletAccount
+    {
+        public static string GenerateFormattedAccountNumber()
+        {
+            string accountNumber = Generate14DigitAccountNumber();
+            return $"{accountNumber.Substring(0, 4)}-{accountNumber.Substring(4, 4)}-{accountNumber.Substring(8, 6)}";
+        }
+
+        private static string Generate14DigitAccountNumber()
+        {
+            Guid guid = Guid.NewGuid();
+            byte[] guidBytes = guid.ToByteArray();
+            long numericValue = BitConverter.ToInt64(guidBytes, 0) ^ BitConverter.ToInt64(guidBytes, 8);
+            long positiveValue = Math.Abs(numericValue);
+            return (positiveValue % 100000000000000).ToString("D14");
+        }
+    }
+}
