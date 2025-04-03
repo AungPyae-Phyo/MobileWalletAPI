012692af7f6beb724936f66043ec8d8a4b707d99
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-03 16:48:44 +0630

    Create DocumentType.cs

---
+++ Domain/Enums/DocumentType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum DocumentType
+    {
+        NRC,
+        PASSPORT,
+        DRIVER_LICENSE,
+        UTILITY_BILL,
+        BANK_STATEMENT,
+        PAYSLIP,
+        TAX_DOCUMENT,
+        BIRTH_CERTIFICATE,
+        MARRIAGE_CERTIFICATE,
+        RESIDENCE_PERMIT,
+        WORK_PERMIT,
+        OTHER
+    }
+}
