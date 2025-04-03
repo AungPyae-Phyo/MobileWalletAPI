fe78c8758f5b0500b7d5db327d7466b23035fd2c
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-04 02:30:29 +0630

    Create TransactionType.cs

---
+++ Domain/Enums/TransactionType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum TransactionType
+    {
+        DEPOSIT,
+        WITHDRAWAL,
+        TRANSFER,
+        LOAN,
+        PAYMENT,
+        REFUND,
+        EXCHANGE,
+        INVESTMENT,
+        DIVIDEND,
+        INTEREST,
+        FEE,
+        CHARGE,
+        REWARD,
+        BONUS,
+        COMMISSION,
+        DISCOUNT,
+        GIFT_CARD
+    }
+}
