38ad104e160904024c28fb582a0e033ba22ad8ca
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-14 02:41:28 +0630

    create: custom query forTransaction

---
+++ Infrastructure/IRepository/ITransactionRepo.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.TransactionDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.IRepository
+{
+    public interface ITransactionRepo
+    {
+        Task<List<TransactionResponseDTO>> GetAllForAdmin();
+    }
+}
---
+++ Infrastructure/Repository/TransactionRepo.cs
@@ -0,0 +1,44 @@
+﻿using Application.DTOs.TransactionDTO;
+using Dapper;
+using Domain.Contracts;
+using Domain.Database;
+using Infrastructure.GenericRepository;
+using Infrastructure.IRepository;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class TransactionRepo : GenericRepository<Transaction, string>, ITransactionRepo
+    {
+   
+
+        public TransactionRepo(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<TransactionResponseDTO>> GetAllForAdmin()
+        {
+            var sql = @"
+                SELECT 
+                    T.Id,
+                    SUser.name AS SenderName,
+                    RWallet.AccountNumber AS ReceiverAccountNumber,
+                    T.TransactionType,
+                    T.Amount,
+                    T.LastModifiedBy,
+                    T.LastModifiedOn
+                FROM TransactionTable T
+                JOIN Wallet SWallet ON T.SenderWalletId = SWallet.Id
+                JOIN Users SUser ON SWallet.UserId = SUser.Id
+                JOIN Wallet RWallet ON T.ReceiverWalletId = RWallet.Id
+                WHERE T.IsDeleted = 0
+                ORDER BY T.LastModifiedOn DESC;
+            ";
+
+            var result = await _connection.QueryAsync<TransactionResponseDTO>(sql);
+            return result.ToList();
+        }
+    }
+}
