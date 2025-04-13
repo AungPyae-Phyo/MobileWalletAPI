1153905089cc0450a31e51df916d70abafba6e54
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-13 16:46:23 +0630

    Create WalletHistory.cs

---
+++ Domain/Contracts/WalletHistory.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    [Table("WalletHistory")]
+    public class WalletHistory : BaseEntity<string>
+    {
+        public string? WalletId { get; set; }
+        public decimal ChangeAmount { get; set; }
+        public decimal PreviousBalance { get; set; }
+        public decimal NewBalance { get; set; }
+    }
+}
