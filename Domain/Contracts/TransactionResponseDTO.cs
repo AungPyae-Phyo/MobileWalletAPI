e3376bc6ac07d79922f24f47e4199e9ee7ce52e3
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-14 02:40:37 +0630

    Create TransactionResponseDTO.cs

---
+++ Domain/Contracts/TransactionResponseDTO.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.TransactionDTO;
+using Domain.Contracts;
+using Domain.Enums;
+using System.Text.Json.Serialization;
+
+
+namespace Application.DTOs.TransactionDTO
+{
+   public class TransactionResponseDTO
+    {
+        public string Id { get; set; }
+
+        public string SenderName { get; set; }
+
+        public string ReceiverAccountNumber { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TransactionType? TransactionType { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime LastModifiedOn { get; set; }
+    }
+}
