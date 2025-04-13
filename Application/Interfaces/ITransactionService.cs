036565f8e02aead508ce813b3d8b5084bd67ec14
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-14 02:41:58 +0630

    Create ITransactionService.cs

---
+++ Application/Interfaces/ITransactionService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.TransactionDTO;
+using Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ITransactionService
+    {
+        Task<string> CreateTransaction(TransactionRequestDTO dto);
+        Task<IEnumerable<Transaction>> GetAll();
+        Task<int> CountAll();
+    }
+}
