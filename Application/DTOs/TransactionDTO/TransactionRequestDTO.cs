5f380e0287429d3a47e01bc5b838a5bde12a9e4a
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-14 02:41:46 +0630

    Create TransactionRequestDTO.cs

---
+++ Application/DTOs/TransactionDTO/TransactionRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TransactionDTO
+{
+    public class TransactionRequestDTO
+    {
+        public string SenderWalletId { get; set; }
+
+        public string ReceiverAccountNumber { get; set; } 
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TransactionType? TransactionType { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
