44f6cc30ece21598bbec0e323d48ab9cd2d667c9
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-15 23:40:09 +0630

    Create LimitFeesDTO.cs

---
+++ Application/DTOs/LimitFeesDTO/LimitFeesDTO.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTOs.LimitFeesDTO;
+using AutoMapper;
+using Domain.Contracts;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.LimitFeesDTO
+{
+    public class LimitFeesDTO
+    {
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TransactionType? TransactionType { get; set; }
+        public string? description { get; set; }
+        public decimal? min_amount { get; set; }
+        public decimal? max_amount { get; set; }
+        public decimal? percent_fees { get; set; }
+    }
+}
+
+public class LimitFeesProfile : Profile
+{
+    public LimitFeesProfile()
+    {
+        // Map LimitFees <-> LimitFeesDTO
+        CreateMap<LimitFees, LimitFeesDTO>().ReverseMap();
+    }
+}
