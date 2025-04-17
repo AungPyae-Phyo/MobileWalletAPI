a466f3b4906906cde0045b69477c64eb88d8f7e3
AungPyae-Phyo <pyaea978@gmail.com>
2025-04-17 23:46:09 +0630

    Create ILimitFeesService.cs

---
+++ Application/Interfaces/ILimitFeesService.cs
@@ -0,0 +1,15 @@
+﻿
+using Application.DTOs.LimitFeesDTO;
+using Domain.Contracts;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Application.Interfaces
+{
+    public interface ILimitFeesService
+    {
+        Task<int> Create(LimitFeesDTO limitFeesDto);
+        Task<int> CountAll();
+        Task<IEnumerable<LimitFees>> GetAll();
+        Task<int> Update(LimitFees limitFees);
+    }
+}
