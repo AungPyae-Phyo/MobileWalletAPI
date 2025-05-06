ea7a3dcd7dad93af988047da60faa1731de0619e
AungPyae-Phyo <pyaea978@gmail.com>
2025-05-06 19:45:57 +0630

    Create TransactionHistoryDto.cs

---
+++ Application/DTOs/TransactionDTO/TransactionHistoryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TransactionDTO
+{
+    public class TransactionHistoryDto
+    {
+        public string Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+        public decimal Amount { get; set; }
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+    }
+}
